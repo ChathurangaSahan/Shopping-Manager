80c6ab69abe4a0ee312ebaeb6056d18864f8e170
Haputhanthrige Sahan Chathuranga Samarasinghe <chathurangasahan2002@gmail.com>
2024-04-01 11:00:24 +0530

    Add files via upload

---
+++ Java_CW/src/Main.java
@@ -0,0 +1,10 @@
+//IIT No: 20220334
+//UoW No: w1953208
+//Name  : H.S.C.Samarasinghe
+
+public class Main {
+    public static void main(String[] args) {
+        //running main interface
+        WestministerShoppingManager asd = new WestministerShoppingManager();
+    }
+}
